73e90af1b2fbd1b019eea87986b7579581388bd3
Erick Firmo <erickfirmo1996@gmail.com>
2022-02-08 13:55:50 -0300

    add code in files types: .php,.css,.map,.log,.js,.scss,.json,.txt remove code in files types:

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
 console.log("cqugn")
 console.log("cbrvo")
 console.log("sftti")
+console.log("yvcea")
--- content.php
+++ content.php
@@ -130,4 +130,5 @@
         echo "hctyh";
         echo "rqhlo";
         echo "suubq";
+        echo "wqblk";
 ?>
